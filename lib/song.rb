098b3f6369f94771fc3f78885ffb07c04365a4ff
Hailey Bongo <haileyebongo@gmail.com>
2020-02-21 21:41:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song 
+  attr_accessor :name, :genre, :artist 
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist 
+    @@all << self
+  end
