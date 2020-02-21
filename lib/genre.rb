4270b2676a137c48b35f8aba91d78fdba6e7f1eb
Hailey Bongo <haileyebongo@gmail.com>
2020-02-21 21:38:50 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,5 +2,13 @@ class Genre
   
   attr_accessor :name 
   
+  @@all = []
+  
     def initialize(name)
       @name = name 
+      @@all << self
+    end
+    
+    def self.all
+      @@all
+    end
