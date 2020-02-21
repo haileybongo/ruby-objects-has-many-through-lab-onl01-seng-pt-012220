ccd83f70a5dc28badc133c9bc71ac658a3e9d7af
Hailey Bongo <haileyebongo@gmail.com>
2020-02-21 22:01:32 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+class Appointment 
+  attr_accessor :name, :genre, :artist 
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
